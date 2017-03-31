42af9e9729be3dbb7ea62b8d6436ee131bd02a4c
rock619 <rock619@gmail.com>
2017-03-31 04:56:44 +0000

    UserにNameの追加 Name, Emailどちらでもログイン可

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,21 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   has_many :tweets, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330061203) do
+ActiveRecord::Schema.define(version: 20170331043524) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20170330061203) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                                null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
